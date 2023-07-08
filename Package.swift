12eaa4b3f6fddee44a6004006c93815dffdba164
Alex Ehlke <alex.ehlke@gmail.com>
2023-07-08 14:06:59 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -5,24 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "LakeImage",
+    platforms: [.macOS(.v12), .iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LakeImage",
             targets: ["LakeImage"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kean/Nuke.git", branch: "main"),
+        .package(url: "https://github.com/lake-of-fire/swift-url.git", branch: "main"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LakeImage",
-            dependencies: []),
-        .testTarget(
-            name: "LakeImageTests",
-            dependencies: ["LakeImage"]),
+            dependencies: [
+                .product(name: "Nuke", package: "Nuke"),
+                .product(name: "NukeUI", package: "Nuke"),
+                .product(name: "WebURL", package: "swift-url"),
+                .product(name: "WebURLFoundationExtras", package: "swift-url"),
+            ]),
+//        .testTarget(
+//            name: "LakeImageTests",
+//            dependencies: ["LakeImage"]),
     ]
 )
